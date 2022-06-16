5b6ad9b01ae41278241fa3d048004bb8d61dd1c5
Patrick Felschen <patrick@felschen.de>
2022-06-16 12:02:35 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module de.hsos.ooadproject {
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.web;
+
+  requires org.controlsfx.controls;
+  requires com.dlsc.formsfx;
+  requires org.kordamp.ikonli.javafx;
+  requires org.kordamp.bootstrapfx.core;
+  requires eu.hansolo.tilesfx;
+
+  opens de.hsos.ooadproject to javafx.fxml;
+  exports de.hsos.ooadproject;
+}
