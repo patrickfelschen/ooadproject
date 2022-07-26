7d98b8f575ecdbd46cc4c435d8cba9ddeecdf198
Julian Voss <julian.voss@hs-osnabrueck.de>
2022-07-26 14:34:38 +0200

    StockListTable search bar

--- src/main/java/de/hsos/ooadproject/controller/StockListTableController.java
+++ src/main/java/de/hsos/ooadproject/controller/StockListTableController.java
@@ -1,23 +1,29 @@
 package de.hsos.ooadproject.controller;
 
+import de.hsos.ooadproject.MainApp;
 import de.hsos.ooadproject.Router;
 import de.hsos.ooadproject.api.StockManager;
 import de.hsos.ooadproject.api.UserManager;
 import de.hsos.ooadproject.datamodel.Stock;
+import de.hsos.ooadproject.interfaces.Routable;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import io.github.palexdev.materialfx.enums.ButtonType;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class StockListTableController implements Initializable {
@@ -26,6 +32,8 @@ public class StockListTableController implements Initializable {
   private TableColumn<Stock, String> colName, colSymbol, colVortag, colBid, colAsk, colPercent, colPlusMinus, colTime, colAction;
   @FXML
   private TableView<Stock> stockListTable;
+  @FXML
+  private MFXTextField searchField;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
@@ -82,7 +90,30 @@ public class StockListTableController implements Initializable {
       return row;
     });
 
-    stockListTable.setItems(stockList);
+    FilteredList<Stock> filteredStockList = new FilteredList<>(stockList);
+
+    this.searchField.textProperty().addListener((observable, oldValue, newValue) -> {
+      filteredStockList.setPredicate(stock -> {
+        if(newValue.isEmpty() || newValue.isBlank()) {
+          return true;
+        }
+
+        String searchKeyword = newValue.toLowerCase();
+
+        if(stock.getName().toLowerCase().contains(searchKeyword)) {
+          return true;
+        }
+        else if(stock.getSymbol().toLowerCase().contains(searchKeyword)) {
+          return true;
+        }
+        else {
+          return false;
+        }
+      });
+    });
+
+    stockListTable.setItems(filteredStockList);
+
   }
 
   void showSockDetailsScreen(Stock stock) throws IOException {
