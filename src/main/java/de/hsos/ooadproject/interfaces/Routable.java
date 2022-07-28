a1aa39aa40363ebd2f93f69653c3b50060ad2e3f
patrickfelschen <patrick@felschen.de>
2022-07-28 17:15:44 +0200

    comments

--- src/main/java/de/hsos/ooadproject/interfaces/Routable.java
+++ src/main/java/de/hsos/ooadproject/interfaces/Routable.java
@@ -1,5 +1,8 @@
 package de.hsos.ooadproject.interfaces;
 
+/**
+ * Abstrakte Klasse um beim Navigieren Daten zu übergeben.
+ */
 public abstract class Routable {
   public void setData(Object data) {
   }
