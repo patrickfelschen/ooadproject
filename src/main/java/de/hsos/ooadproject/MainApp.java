643ee2e5ff8abe91e1de19216c98089451c2eb06
Julian Voss <julian.voss@hs-osnabrueck.de>
2022-07-29 17:36:51 +0200

    app icon

--- src/main/java/de/hsos/ooadproject/MainApp.java
+++ src/main/java/de/hsos/ooadproject/MainApp.java
@@ -1,12 +1,17 @@
 package de.hsos.ooadproject;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     stage.setTitle("Aktienverwaltung");
+
+    Image image = new Image(String.valueOf(MainApp.class.getResource("img/1024.png")));
+    stage.getIcons().add(image);
+
     Router.bind(stage);
     Router.getInstance().pushRoute("watchList");
   }
